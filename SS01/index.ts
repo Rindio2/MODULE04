3059031943d145b7b37bec7782fc7e4bbce20b1f
Rindio <tuanspacework@gmail.com>
2024-05-13 21:11:32 +0700

    MODULE04

---
+++ SS01/index.ts
@@ -0,0 +1,75 @@
+
+// bafi 1
+// let name1:string, age:number, job:string ;
+// name1 = "hoa";
+// age= 21;
+// job= "DEV";
+// console.log("11111",name1,age,job);
+
+// bafi 2
+// let userName:string[];
+// userName = ["tuan","tuna"];
+// console.log("11111",userName);
+
+// bafi 3
+// let a:number,b:number,c:number;
+// a =2;
+// b =3;
+// c = a+b;
+// console.log("111",c);
+
+// bafi 4
+// let check:boolean = true;
+// if (check) {
+//     console.log("xin chao");
+    
+// } else {
+//     console.log("tam biet"); 
+// }
+
+// bafi 5
+// let a:undefined, b:undefined, c:number;
+// console.log(a, b);
+// console.log(c); // vì c chưa được gán giá trị nên được cho là undefined
+
+// bai 6
+// let a:string = "2";
+// let b:number = 2;
+// if (a=b) {
+//     console.log("true");
+    
+// } else {
+//     console.log("false"); 
+// } //Type 'number' is not assignable to type 'string'
+
+// bafi 7
+// let num1:number,num2:number,num3:number
+// let num1:string,num2:boolean,num3:number
+// num1 =2;
+// num2 =5;
+// num3 = num1 +num2;
+// console.log("222",num3); // xay ra loi
+
+//bafi 8
+// let firstName:string = "john";
+// let lastName:string = "doe";
+// firstName = firstName.charAt(0).toUpperCase() + firstName.slice(1);
+// lastName = lastName.charAt(0).toUpperCase() + lastName.slice(1);
+// let fullName: string = firstName + " " + lastName;
+// console.log(fullName);
+
+// bafi 9
+// let inputString: string = "banana";
+// let uniqueCharacters: string = "";
+// for (let i = 0; i < inputString.length; i++) {
+//     if (uniqueCharacters.indexOf(inputString[i]) === -1) {
+//         uniqueCharacters += inputString[i];
+//     }
+// }
+// console.log(uniqueCharacters);
+
+// bafi 10
+
+
+
+
